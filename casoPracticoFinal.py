70144e0c1f2fdd93abb8680dfc07a4a96747f655
PedroSwagf <pfernandezch02@gmail.com>
2024-05-16 15:52:45 +0200

    Version 1.1

--- casoPracticoFinal.py
+++ casoPracticoFinal.py
@@ -1,5 +1,8 @@
+#importamos las variables de sistema
 import os
 import time
+
+os.system("cls")
 os.system("color B")
 #Definimos las clases para las tareas
 class Tarea:
@@ -14,23 +17,29 @@ class GestorTareas:
     def agregar_tareas(self, descripcion):
         tarea = Tarea(descripcion)
         self.tareas.append(tarea)
+        os.system("Color 2")
         print("Tarea agregada correctamente.")
-
+        time.sleep(2)
+        os.system("Color B")
     def marcar_completadas(self, posicion):
         try:
             tarea = self.tareas[posicion]
             tarea.completada = True
+            os.system("Color A")
             print("La Tarea se marco como completada correctamente.")
+            time.sleep(2)
+            os.system("Color B")    
         except IndexError:
-            os.system("color 5")
+            os.system("Color D")
             print("La posicion especificada no existe.")
-            time.sleep(10)
-            os.system("color 7")
+            time.sleep(2)
+            os.system("Color B")
     def mostrar_tareas(self):
         if not self.tareas:
-            os.system("color 4")
+            os.system("Color E")
             print("No hay tareas pendientes.")
-            os.system("color B")
+            time.sleep(2)
+            os.system("Color B")
         else:
             for i, tarea in enumerate(self.tareas):
                 estado = "Completada" if tarea.completada else "Pendiente"
@@ -39,10 +48,15 @@ class GestorTareas:
     def eliminar_tareas(self, posicion):
         try:
             del self.tareas[posicion]
+            os.system("Color A")
             print("Tarea eliminada correctamente.")
+            time.sleep(2)
+            os.system("Color B")
         except IndexError:
+            os.system("Color C")
             print("La posicion especificada no existe.")
-
+            time.sleep(2)
+            os.system("Color B")
 # Funcion principal para interactuar con el usuario
 def main():
     
@@ -51,16 +65,18 @@ def main():
     gestor = GestorTareas()
 
     while True:
-        print("╔════════════════════════════════╗")
-        print("║ Opciones a realizar:           ║")
-        print("║════════════════════════════════║")
-        print("║ 1. Agregar nueva tarea         ║")
-        print("║ 2. Marcar tarea como completada║")
-        print("║ 3. Mostrar todas las tareas    ║")
-        print("║ 4. Eliminar tarea              ║")
-        print("║ 5. Salir                       ║")
-        print("╚════════════════════════════════╝")
+        print("                                       ╔════════════════════════════════╗")
+        print("                                       ║ Opciones a realizar:           ║")
+        print("                                       ║════════════════════════════════║")
+        print("                                       ║ 1. Agregar nueva tarea         ║")
+        print("                                       ║ 2. Marcar tarea como completada║")
+        print("                                       ║ 3. Mostrar todas las tareas    ║")
+        print("                                       ║ 4. Eliminar tarea              ║")
+        print("                                       ║ 5. Salir                       ║")
+        print("                                       ╚════════════════════════════════╝")
 
+        print("")
+        print("")
         opcion = input("Seleccione una opcion: ")
 
         if opcion == "1":
